a0f0c47b1f80a3bef487dca34f5390ab4e3eeba8
SaraReneeRosario <sarareneerosario@gmail.com>
2020-05-09 20:43:35 -0700

    APIs are hard

---
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,2 @@
+# Google API Key
+g_key = "AIzaSyBBx4Hvy5pSsSs9t5_xsKU0TsHpoJ_D4Ek"
---
+++ starter_code/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "292098f08813ea72e057e11bb083e4ae"
+
+# Google API Key
+g_key = "AIzaSyBBx4Hvy5pSsSs9t5_xsKU0TsHpoJ_D4Ek"
